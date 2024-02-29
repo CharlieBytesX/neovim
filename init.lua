0a2ab22490afbd8e60f0febaf9c17f75d6e983cf
Carlos Solórzano Cerdas <62361574+charlieman2700@users.noreply.github.com>
2024-02-29 02:25:59 -0400

    Clean init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -30,8 +28,6 @@ local colorscheme = colorschemes.RosepineMoon
 
 vim.cmd('colorscheme ' .. colorscheme)
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
 require("mappings")
 if COLEMAK_MODE then
   require("colemak")
