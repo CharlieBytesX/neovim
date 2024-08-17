5d490299f9c5b74b1945b8170acd30875d69efd4
Carlos Solórzano Cerdas <csolorzano@istmocenter.com>
2024-08-17 09:18:29 -0600

    feat: Rust test are now runned smooth with number selection or normal/arrow selction

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -75,71 +75,76 @@ function SelectAndRunTest()
 
   -- Add an option to run all tests
   table.insert(tests, 1, "Run all tests")
+
+  -- Prefix each test with a number for easier selection
+  local numbered_tests = {}
+  for i, test in ipairs(tests) do
+    table.insert(numbered_tests, string.format("%d. %s", i, test))
+  end
+
   -- Telescope picker for selecting a test
   pickers.new({}, {
     prompt_title = "Select Test to Run",
     finder = finders.new_table({
-      results = tests
+      results = numbered_tests
     }),
     sorter = conf.generic_sorter({}),
     attach_mappings = function(prompt_bufnr, map)
-      -- Define what happens on selection
+      -- Default action (selection via arrow keys or input) handling
       actions.select_default:replace(function()
-        local choice = action_state.get_selected_entry()
+        -- Get the currently selected item (the highlighted item)
+        local selected_entry = action_state.get_selected_entry()
         actions.close(prompt_bufnr)
 
-        if choice == nil then
-          print("Invalid selection.")
-          return
-        end
+        -- Extract the selected number from the displayed entry
+        local choice = tonumber(selected_entry.value:match("^(%d+)"))
 
-        -- Run the selected test or all tests
-        if choice.index == 1 then
+        if choice == 1 then
           -- Run all tests in the current file
           local file_name = GetCurrentFileNameForRust()
           if file_name then
-            vim.cmd("tabnew | terminal cargo test  " .. file_name)
+            vim.cmd("tabnew | terminal cargo test " .. file_name)
           else
             print("Could not determine file name.")
           end
         else
           -- Run the selected test
-          local selected_test = choice.value
-          vim.cmd("vsplit | terminal cargo test " .. selected_test)
+          local selected_test = tests[choice]
+          vim.cmd("split | terminal cargo test " .. selected_test)
+        end
+      end)
+
+      -- Handle pressing Enter in insert mode after typing a number or navigating
+      map('i', '<CR>', function()
+        local prompt_text = action_state.get_current_line()
+        local selected_number = tonumber(prompt_text)
+
+        if selected_number and selected_number >= 1 and selected_number <= #numbered_tests then
+          actions.close(prompt_bufnr)
+
+          -- Run the selected test or all tests
+          if selected_number == 1 then
+            -- Run all tests in the current file
+            local file_name = GetCurrentFileNameForRust()
+            if file_name then
+              vim.cmd("tabnew | terminal cargo test " .. file_name)
+            else
+              print("Could not determine file name.")
+            end
+          else
+            -- Run the selected test
+            local selected_test = tests[selected_number]
+            vim.cmd("split | terminal cargo test " .. selected_test)
+          end
+        else
+          -- No number was typed; run the currently highlighted option
+          actions.select_default(prompt_bufnr)
         end
       end)
 
       return true
     end,
   }):find()
-
-  -- -- Present a selection prompt to the user
-  -- local options = {}
-  -- for i, test in ipairs(tests) do
-  --   table.insert(options, string.format("%d. %s", i, test))
-  -- end
-  --
-  -- -- Use vim's inputlist to prompt the user to choose a test
-  -- local choice = vim.fn.inputlist(options)
-  --
-  -- if choice < 1 or choice > #tests then
-  --   print("Invalid selection.")
-  --   return
-  -- end
-  -- -- Run the selected test or all tests
-  -- if choice == 1 then
-  --   -- Run all tests in the current file
-  --   local file_name = GetCurrentFileNameForRust()
-  --   if file_name then
-  --     vim.cmd("tabnew | terminal cargo test  " .. file_name)
-  --   else
-  --     print("Could not determine file name.")
-  --   end
-  -- else
-  --   -- Run the selected test
-  --   local selected_test = tests[choice]
-  --   vim.cmd("split | terminal cargo test " .. selected_test)
-  -- end
 end
 
 -- Function to run Rust tests
